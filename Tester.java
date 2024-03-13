5dfff88a38b9a35cd14a0250f5f43e97e02fd7ef
FerCamposo <163018376+LadyFerny@users.noreply.github.com>
2024-03-13 23:12:01 +0100

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -4,9 +4,10 @@ public class Tester {
 
 	public static void main(String[] args) {
 		//Combinacion c1 = new Combinacion();
-		//c1.gereraCSecreta(2, 8);
+		//c1.generaCSecreta(2, 8);
 		Mastermind juego = new Mastermind();
 		juego.iniciarJuego();
+		juego.pedirJugada();
 
 	}
 
